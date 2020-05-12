69962fa75650166c16f05b78bdf856a1795e9ea1
cjlaaa <cjlaaa@gmail.com>
2020-05-12 23:42:10 +0800

    玩家飞船

---
+++ Source/AirplaneWar/Private/SpaceShip.cpp
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpaceShip.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Core/Public/Misc/App.h"
+
+// Sets default values
+ASpaceShip::ASpaceShip()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 设置组件
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	RootComponent = RootComp;
+	ShipSM = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShipSM"));
+	ShipSM->SetupAttachment(RootComponent);
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComp"));
+	CollisionComp->SetupAttachment(RootComponent);
+
+	//// 设置正交相机
+	//CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CamaraComp"));
+	//CameraComp->SetupAttachment(RootComponent);
+	//CameraComp->SetProjectionMode(ECameraProjectionMode::Orthographic);
+	//CameraComp->SetOrthoWidth(2500);
+
+	
+	Speed = 1000;
+}
+
+// Called when the game starts or when spawned
+void ASpaceShip::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// 设置显示鼠标
+	PC = Cast<APlayerController>(GetController());
+	PC->bShowMouseCursor = true;
+}
+
+// 指向鼠标方向
+void ASpaceShip::LookAtCursor()
+{
+	FVector MouseLocation, MouseDirection;
+	// 获取鼠标的位置和方向并转为世界坐标
+	PC->DeprojectMousePositionToWorld(MouseLocation, MouseDirection);
+	// 只需X和Y旋转,Z不变
+	FVector TargetLocation = FVector(MouseLocation.X, MouseLocation.Y, GetActorLocation().Z);
+	// 获取指向目标方向的旋转角度
+	FRotator Rotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), TargetLocation);
+	SetActorRotation(Rotation);
+}
+
+void ASpaceShip::MoveUp(float Value)
+{
+	AddMovementInput(FVector::ForwardVector, Value);
+}
+
+void ASpaceShip::MoveRight(float Value)
+{
+	AddMovementInput(FVector::RightVector, Value);
+}
+
+void ASpaceShip::Move()
+{
+	AddActorWorldOffset(ConsumeMovementInputVector() * Speed * FApp::GetDeltaTime(), true);
+}
+
+// Called every frame
+void ASpaceShip::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	LookAtCursor();
+	Move();
+}
+
+// Called to bind functionality to input
+void ASpaceShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveUp", this, &ASpaceShip::MoveUp);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASpaceShip::MoveRight);
+}
+
---
+++ Source/AirplaneWar/Public/SpaceShip.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "SpaceShip.generated.h"
+
+class USphereComponent;
+class UCameraComponent;
+
+UCLASS()
+class AIRPLANEWAR_API ASpaceShip : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASpaceShip();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+	USceneComponent* RootComp;
+
+	UPROPERTY(VisibleAnywhere,Category="Component")
+	USphereComponent* CollisionComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+	UStaticMeshComponent* ShipSM;
+
+	//UPROPERTY(VisibleAnywhere, Category = "Component")
+	//UCameraComponent* CameraComp;
+
+	APlayerController* PC;
+
+	UPROPERTY(EditAnywhere, Category = "Move")
+	float Speed;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void LookAtCursor();
+
+	void MoveUp(float Value);
+	void MoveRight(float Value);
+	void Move();
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
