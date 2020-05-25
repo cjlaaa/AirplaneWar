b1b7ec5df0342dc94d0684103c7aac83e02a8876
cjlaaa <cjlaaa@gmail.com>
2020-05-25 23:48:15 +0800

    创建游戏模式类,创建分数UI

---
+++ Source/AirplaneWar/Public/ShipGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShipGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIRPLANEWAR_API AShipGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
